4e331b8d7d1d96d9a45cec77b3a7e1bbf0eb18ac
alfiyah0813 <alfiyah_zahra_33rpl@student.smktelkom-mlg.sch.id>
2024-08-03 15:49:10 +0700

    Add files via upload

--- Tes.java
+++ Tes.java
@@ -1,21 +1,5 @@
 public class Tes {
 public static void main (String[]args) {
       System.out.println("ini adalah file Tes");
-      String nama=  "Fifi";
-      String asalKota= "Lumajang";
-      int usia=15;
-      double beratBadan=50.00;
-      float tinggiBadan=152.0f;
-      char jenisKelamin='p';
-      boolean status = false;
-
-System.out.println("Nama: "+nama);
-System.out.println("Usia: "+usia);
-System.out.println("Berat Badan: "+beratBadan);
-System.out.println("Tinggi Badan: "+tinggiBadan);
-System.out.println("Jenis Kelamin: "+jenisKelamin);
-System.out.println("Status: "+status);
-
-
 }
 }
