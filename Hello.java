1711a5cbe1828ed8756dd96fac32ffad8cb8e92e
alfiyah0813 <alfiyah_zahra_33rpl@student.smktelkom-mlg.sch.id>
2024-08-03 15:50:13 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("ini adalah file Hello");
+        String nama= "Fifi";
+        String asalKota= "Lumajang";
+        int usia=15;
+        double beratBadan=50.00;
+        float tinggiBadan=153.0f;
+        char jenisKelamin='p';
+        boolean status = false;
+
+System.out.println("Nama: "+nama);    
+System.out.println("Usia: "+usia);  
+System.out.println("Asal Kota "+asalKota);
+System.out.println("Berat Badan: "+beratBadan);
+System.out.println("Tinggi Badan: "+tinggiBadan);
+System.out.println("Jenis Kelamin: "+jenisKelamin);
+System.out.println("Status: "+status);
+    }
+}
